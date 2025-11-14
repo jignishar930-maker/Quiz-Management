b4e236372a02435d989f8fc4a1341be44ff2502a
jignishar930-maker <jignishar930@gmail.com>
2025-11-14 07:55:46 +0530

    today update

--- qms/src/App.jsx
+++ qms/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./Component/pages/Home"
-
-
+import img1 from './assets/images/img1.jpg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   
 
@@ -8,6 +8,7 @@ function App() {
     <>
     <Home/>
       <h1>hiiiii</h1>
+      <img  src={img1} alt=" "/>
     </>
   )
 }
