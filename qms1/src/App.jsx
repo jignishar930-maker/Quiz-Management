f4e9a90d9b7a6f50e354ba225957be961e872763
karishmarathod545-ai <jignishar930@gmail.com>
2025-11-20 12:55:46 +0530

    20/11/2025

--- qms1/src/App.jsx
+++ qms1/src/App.jsx
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 import Home from "./Component/pages/Home";
-=======
-import Home from "./Component/pages/Home"
-import img1 from './assets/image/img1.jpg';
-import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  
- const [count,setCount] = useState(0)
->>>>>>> 1767ea8f2736d71ae19eb9e040d5e9e339124d80
+  const [count, setCount] = useState(0);
 
-function App() {
   return (
     <>
-<<<<<<< HEAD
+      <Router>
+        <header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+
       <Home />
-=======
-    <Router>
-      <header/>
-      <Routes>
-        <Route path="/" element={<Home/>}>
-        </Route>
-        
-      </Routes>
-    </Router>
-    <Home/>
->>>>>>> 1767ea8f2736d71ae19eb9e040d5e9e339124d80
       <h1>hiiiii</h1>
     </>
   );
--- qms1/src/Component/Layout/header.jsx
+++ qms1/src/Component/Layout/header.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
 import React from "react";
 import img from '../../assets/images/img1.jpg';
 import { Link } from "react-router-dom";
->>>>>>> 1767ea8f2736d71ae19eb9e040d5e9e339124d80
